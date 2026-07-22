ee4051997a7f89f5b63dcf0512b84e7119f58ffa
GuillermoAlcala <alcalamemo@gmail.com>
2026-07-22 15:28:53 -0600

    22jul2026

---
+++ inche we/Views/Task/EditOfficeTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  EditOfficeTaskView.swift
+//  inche we
+//
+//  Created by GuillermoChaconAlcala on 22/07/26.
+//
+
+import SwiftUI
+
+struct EditOfficeTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditOfficeTaskView()
+}
