6452e63528c8016bac64ddaa52346791d0510e40
GuillermoAlcala <alcalamemo@gmail.com>
2026-07-29 22:44:44 -0600

    Se agrega funcionalidad similar a la de Office View, así como validación en el boton Save antes de guardar. 29072026

---
+++ inche we/Views/Personal_OfficeView/EditPersonalTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPersonalTaskView.swift
+//  inche we
+//
+//  Created by GuillermoChaconAlcala on 26/07/26.
+//
+
+import SwiftUI
+
+struct EditPersonalTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPersonalTaskView()
+}
